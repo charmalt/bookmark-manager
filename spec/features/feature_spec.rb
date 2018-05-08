95608f9c115afaa309c27b5ffdacca16f3857691
Charlene <charlenechetcuti@gmail.com>
2018-05-08 17:52:16 +0100

    minor refactor

--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -3,7 +3,7 @@ feature 'BookmarkManager' do
   scenario 'Click button to view links' do
     visit('/')
     con = PG.connect :dbname => 'bookmark_manager_test', :user => 'charlenechetcuti'
-    con.exec "INSERT INTO bookmarks (url) VALUES ('http://test.com');"
+    con.exec "INSERT INTO bookmarks (url) VALUES ('http://test.com')"
     click_button 'View Links'
     expect(page).to have_content('http://test.com')
   end
