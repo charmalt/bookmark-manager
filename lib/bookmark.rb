1a2be63e248c6a191a484a64ca3afc347191fb62
Charlene <charlenechetcuti@gmail.com>
2018-05-08 17:35:01 +0100

    add test database and use rack env to run test database

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -4,9 +4,16 @@ class Bookmark
   attr_reader :url
 
   def self.all
-    begin
-      con = PG.connect :dbname => 'bookmark_manager', :user => 'John'
-      con.exec "SELECT * FROM bookmarks"
+    if (ENV['RACK_ENV'] == 'test')
+      begin
+        con = PG.connect :dbname => 'bookmark_manager_test', :user => 'charlenechetcuti'
+        con.exec "SELECT * FROM bookmarks"
+      end
+    else
+      begin
+        con = PG.connect :dbname => 'bookmark_manager', :user => 'charlenechetcuti'
+        con.exec "SELECT * FROM bookmarks"
+      end
     end
   end
 
